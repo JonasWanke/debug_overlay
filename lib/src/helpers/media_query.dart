f1b748c059a6a5eae0bec83e1ea59ac4b79fea13
Jonas Wanke <contact@jonas-wanke.com>
2023-05-16 11:53:20 +0200

    Migrate away from window singleton

--- lib/src/helpers/media_query.dart
+++ lib/src/helpers/media_query.dart
@@ -16,7 +16,7 @@ class MediaQueryDebugHelper extends StatelessWidget {
   Future<List<DiagnosticsNode>> _getDiagnostics(BuildContext context) async {
     var data = MediaQuery.maybeOf(context);
     if (data == null) {
-      data = MediaQueryData.fromWindow(WidgetsBinding.instance.window);
+      data = MediaQueryData.fromView(View.of(context));
       if (!kReleaseMode) {
         data = data.copyWith(platformBrightness: debugBrightnessOverride);
       }
