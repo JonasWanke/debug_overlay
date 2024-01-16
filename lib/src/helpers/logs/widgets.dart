a27a422f08437887ce8dcba17fc7c5e127aa1e56
Jonas Wanke <contact@jonas-wanke.com>
2024-01-16 07:28:42 +0100

    Fix linter complaint

--- lib/src/helpers/logs/widgets.dart
+++ lib/src/helpers/logs/widgets.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
+// TODO(JonasWanke): Remove the import when upgrading Flutter
+// ignore: unnecessary_import
 import 'dart:ui';
 
 import 'package:black_hole_flutter/black_hole_flutter.dart';
